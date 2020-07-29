47411d93a2656ed1f2881ea81ef6bc1125ebd7aa
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-07-29 18:22:28 +1000

    add usestate to collapse navBar onSelect

--- client/src/components/MyNavBar/myNavBar.js
+++ client/src/components/MyNavBar/myNavBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Navbar, Nav } from 'react-bootstrap';
 import LogoImage from '../assets/images/logo_high_resolution_white.png';
 import "./style.css";
@@ -6,64 +6,69 @@ import { Link, animateScroll as scroll } from "react-scroll";
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
-class MyNavBar extends Component {
-    scrollToTop = () => {
+function MyNavBar() {
+    const [expanded, setExpanded] = useState(false);
+
+    function scrollToTop() {
         scroll.scrollToTop();
     };
-    render() {
-        return (
-            <Navbar variant='dark' style={{ backgroundColor: "#212121" }} expand="lg" className='header'>
-                <Navbar.Brand className='navbar-brand' href="/">
-                    <img src={ LogoImage } alt="beyondLogo" className="logoImage" onClick={this.scrollToTop} />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto nav-item">
-                        <Link
-                            className="nav-link"
-                            href='/aboutUs'
-                            activeClass="active"
-                            to="aboutUs"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                        >ABOUT US</Link>
-                        <Link
-                            className="nav-link"
-                            href='/services'
-                            activeClass="active"
-                            to="services"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                        >SERVICES</Link>
-                        <Link
-                            className="nav-link"
-                            href='/contact'
-                            activeClass="active"
-                            to="contact"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                        >CONTACT</Link>
-                        <Link
-                            className="nav-link"
-                            href='/clientLogin'
-                            activeClass="active"
-                            to="clientLogin"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                        >CLIENT LOGIN</Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+
+    return (
+        <Navbar variant='dark' style={{ backgroundColor: "#212121" }} expand="lg" className='header' expanded={expanded}>
+            <Navbar.Brand className='navbar-brand' href="/">
+                <img src={LogoImage} alt="beyondLogo" className="logoImage" onClick={scrollToTop} />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto nav-item">
+                    <Link
+                        className="nav-link"
+                        href='/aboutUs'
+                        activeClass="active"
+                        to="aboutUs"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        onClick={() => setExpanded(false)}
+                    >ABOUT US</Link>
+                    <Link
+                        className="nav-link"
+                        href='/services'
+                        activeClass="active"
+                        to="services"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        onClick={() => setExpanded(false)}
+                    >SERVICES</Link>
+                    <Link
+                        className="nav-link"
+                        href='/contact'
+                        activeClass="active"
+                        to="contact"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        onClick={() => setExpanded(false)}
+                    >CONTACT</Link>
+                    <Link
+                        className="nav-link"
+                        href='/clientLogin'
+                        activeClass="active"
+                        to="clientLogin"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        onClick={() => setExpanded(false)}
+                    >CLIENT LOGIN</Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
 
 export default MyNavBar;
