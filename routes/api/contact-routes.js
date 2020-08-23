a39e44c5ec7e5633a2048f40ce15cf185ab8ea2a
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-08-23 23:01:08 +1000

    get rid of createTestAccount

--- routes/api/contact-routes.js
+++ routes/api/contact-routes.js
@@ -10,8 +10,7 @@ router.use(cors())
 router.post('/api/contactForm', (req, res) => {
     const inputData = req.body;
     console.log(inputData);
-    nodemailer.createTestAccount((err, account) => {
-        const htmlEmail = `
+    const htmlEmail = `
         <h3>Contact Details</h3>
         <ul>
             <li>Name: ${inputData.inputName}</li>
@@ -21,36 +20,38 @@ router.post('/api/contactForm', (req, res) => {
         <p>${inputData.inputMessage}</p>
         `
 
-        let transporter = nodemailer.createTransport({
-            service: 'Office365',
-            host: 'smtp.office365.com',
-            port: 25,
-            secureConnection: false,
-            auth: {
-                user: process.env.NODEMAILER_USER,
-                pass: process.env.NODEMAILER_PASS
-            },
-            tls:{
-                rejectUnauthorized: false
-            }
-        })
-        console.log(transporter.options.auth.user);
-        let mailOptions = {
-            from: transporter.options.auth.user,
-            to: 'info@beyondvs.com.au',
-            replyTo: inputData.inputEmail,
-            subject: 'New Contact from Beyondvs Website',
-            text: inputData.inputMessage,
-            html: htmlEmail
+    let transporter = nodemailer.createTransport({
+        host: 'smtpout.secureserver.net',
+        port: 465,
+        secure: true,
+        secureConnection: false,
+        tls: {
+            ciphers: 'SSLv3',
+            // rejectUnauthorized: false
+        },
+        requireTLS: true,
+        debug: true,
+        auth: {
+            user: process.env.NODEMAILER_USER,
+            pass: process.env.NODEMAILER_PASS
         }
+    })
+    console.log(transporter.options.auth.user);
+    let mailOptions = {
+        from: transporter.options.auth.user,
+        to: transporter.options.auth.user,
+        replyTo: inputData.inputEmail,
+        subject: 'New Contact from Beyondvs Website',
+        text: inputData.inputMessage,
+        html: htmlEmail
+    }
 
-        transporter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                return console.log(err);
-            }
-            console.log('Message sent: %s', info);
-            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info));
-        })
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Message sent: %s', info);
+        console.log('Message URL: %s', nodemailer.getTestMessageUrl(info));
     })
 
     if (inputData) {
